3f708e96fd4b2430c77403a7e7cac00a1b1c0709
Inthuyen <inaguleswaran@torontomu.ca>
2023-09-08 16:57:45 -0400

    changes

--- process_survey.php
+++ process_survey.php
@@ -1,9 +1,25 @@
 <?php
-// Retrieve form data
-$rating = $_POST['customRange2'];
-$feedback = $_POST['Feedback'];
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-// Store the data in a SQL database
+    include 'connection.php';
 
-exit();
+    // Retrieve form data
+    $rating = $_POST['rating'];
+    $feedback = $_POST['feedback'];
+
+    // Store the data in a SQL database
+    $sql = "INSERT INTO surveys (rating, feedback) VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $rating, $feedback);
+
+    if ($stmt->execute()) {
+        echo "Report submitted successfully.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+    
 ?>
